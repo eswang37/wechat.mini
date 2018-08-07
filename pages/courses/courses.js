c460618fc66ec3c2384db134e55365f0c3d25b40
Jun Wang <andmap.co@gmail.com>
2018-08-06 18:18:18 -0700

    initial

---
+++ pages/courses/courses.js
@@ -0,0 +1,124 @@
+// pages/courses/courses.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    courses: [{
+        "title": "Geometry",
+        "title_cn": "几何",
+        "price": "$30"
+      },
+      {
+        "title": "Algebra",
+        "title_cn": "代数",
+        "price": "$30"
+      },
+      {
+        "title": "Math 6-8",
+        "title_cn": "中学数学",
+        "price": "$30"
+      },
+      {
+        "title": "Math 1-5",
+        "title_cn": "小学数学",
+        "price": "$30"
+      },
+      {
+        "title": "Writing",
+        "title_cn": "英语写作",
+        "price": "$30"
+      },
+      {
+        "title": "Reading",
+        "title_cn": "英语阅读",
+        "price": "$30"
+      },
+      {
+        "title": "Listening",
+        "title_cn": "英语听力",
+        "price": "$30"
+      },
+      {
+        "title": "Speaking",
+        "title_cn": "英语听说",
+        "price": "$30"
+      },
+      {
+        "title": "History",
+        "title_cn": "历史",
+        "price": "$30"
+      },
+      {
+        "title": "Science",
+        "title_cn": "科学",
+        "price": "$30"
+      },
+      {
+        "title": "Soccer",
+        "title_cn": "足球",
+        "price": "$35"
+      }
+    ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.setData({
+     
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,35 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Tutoring',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../courses/courses'
+    })
+  },
+  onLoad: function () {
+    
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  onShareAppMessage: function () {
+    return {
+      title: 'Tutoring 英语数学',
+      path: 'pages/index/index'
+    }
+  }
+})
